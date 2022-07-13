eb9204313b2eefea6c26aaf0512a7d0f651eb738
Fabian Piper <fabianpiper@web.de>
2022-07-13 16:12:45 +0200

    feat: add tailwind

--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <img alt="Vue logo" src="@/assets/images/logo.png" />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
