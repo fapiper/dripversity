e7b4576143c7d2bc7af23164720f4c8ddbcea85d
Fabian Piper <fabianpiper@web.de>
2022-07-25 12:36:14 +0200

    fix: service worker caching

--- packages/app/vue.config.js
+++ packages/app/vue.config.js
@@ -1,4 +1,17 @@
+const manifestJSON = require("./public/manifest.json");
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
     transpileDependencies: true,
+    pwa: {
+        name: manifestJSON.short_name,
+        themeColor: manifestJSON.theme_color,
+        msTileColor: manifestJSON.background_color,
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
+        workboxPluginMode: "InjectManifest",
+        workboxOptions: {
+            swSrc: "./service-worker.js",
+        },
+    },
 });
