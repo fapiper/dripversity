c81f619627aed40714ff06784cbb6f7b3500683d
Fabian Piper <fabianpiper@web.de>
2022-10-25 12:13:29 +0200

    fix: theme color

--- packages/app/src/components/token/TokenCard.vue
+++ packages/app/src/components/token/TokenCard.vue
@@ -10,15 +10,15 @@
             class="absolute top-0 left-0 w-full h-full flex p-2 bg-neutral-900/90 backdrop-blur-sm rounded opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-200"
         >
             <div
-                class="p-2 md:p-4 border-2 border-neutral-400/50 border-dashed flex flex-col justify-center space-y-2 md:space-y-4"
+                class="p-4 border-2 border-neutral-400/50 border-dashed flex flex-col justify-center space-y-4"
             >
                 <template v-if="isPreSale">
                     <h4
-                        class="font-bold text-neutral-400/50 text-3xl md:text-6xl lg:text-7xl uppercase"
+                        class="font-bold text-neutral-400/50 text-7xl uppercase"
                     >
                         {{ token.name }}
                     </h4>
-                    <p class="text-xs md:text-base text-neutral-400">
+                    <p class="text-neutral-400">
                         Join the waitlist and be the first to claim this NFT on
                         launch day
                     </p>
@@ -28,13 +28,11 @@
                 </template>
                 <template v-else>
                     <h4 class="font-bold text-xl">Yours?</h4>
-                    <p class="text-xs md:text-base text-neutral-400">
+                    <p class="text-neutral-400">
                         Mint your own NFT now. Lorem Ipsum dolor sit amet.
                     </p>
                     <div>
-                        <AppButton size="sm" to="/#home"
-                            >Start Minting</AppButton
-                        >
+                        <AppCta size="sm" />
                     </div>
                 </template>
             </div>
